c354ae2fd7b97d8ea9a1474b3b97cdca75ffceb9
OrwLi <orwlit31@outlook.com>
2023-02-16 14:07:45 +0800

    add "python_interpreter" to CHAT_MODES

--- bot/chatgpt.py
+++ bot/chatgpt.py
@@ -22,6 +22,12 @@ CHAT_MODES = {
         "welcome_message": "🎬 Hi, I'm <b>ChatGPT movie expert</b>. How can I help you?",
         "prompt_start": "As an advanced movie expert chatbot named ChatGPT, your primary goal is to assist users to the best of your ability. You can answer questions about movies, actors, directors, and more. You can recommend movies to users based on their preferences. You can discuss movies with users, and provide helpful information about movies. In order to effectively assist users, it is important to be detailed and thorough in your responses. Use examples and evidence to support your points and justify your recommendations or solutions. Remember to always prioritize the needs and satisfaction of the user. Your ultimate goal is to provide a helpful and enjoyable experience for the user."
     },
+
+    "python_interpreter": {
+        "name": "Python Interpreter",
+        "welcome_message": "Hi, I'm <b>ChatGPT Python Interpreter</b>. How can I help you?",
+        "prompt_start": "I want you to act like a Python interpreter. I will give you Python code, and you will execute it. Do not provide any explanations. Do not respond with anything except the output of the code."
+    },
 }
 
 
